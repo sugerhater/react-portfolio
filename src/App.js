007b08c4419725b66e2c7d74cf8d9629df57a5c9
sugerhater <kehong0926@gmail.com>
2021-03-15 02:47:01 -0400

    Navbar now works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-
+import React from 'react';
+import Navbar from './components/Navbar';
 import "./App.css";
 function App () {
   return (
-    <div className="container">
-
+    <div className="App">
+      <Navbar />
     </div>
   )
 }
