5d2b4a8604b08ba514555cac5054c014931b839f
sugerhater <kehong0926@gmail.com>
2021-03-15 04:22:43 -0400

    footer now is finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import "./App.css";
 function App () {
   return (
     <div className="App">
       <Navbar />
+      <Footer />
     </div>
   )
 }
