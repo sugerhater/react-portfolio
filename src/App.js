5c880c2be28cadf896183e22e3e5df7b0644aff4
sugerhater <kehong0926@gmail.com>
2021-03-16 05:03:48 -0400

    updated intro

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Project from "./components/Project";
+import Intro from "./components/Intro";
 import "./App.css";
 // import "./index.css"
 import Portfolio from './components/Portfolio';
@@ -9,6 +10,7 @@ function App () {
   return (
     <div className="App">
       <Navbar />
+      <Intro />
       <Portfolio />
       <Footer />
     </div>
